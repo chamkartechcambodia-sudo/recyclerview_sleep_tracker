5e3d6b81edccd6a8dab82b4c34fcec7aafbfb501
chamkartechcambodia-sudo <chamkartechcambodia@gmail.com>
2026-04-07 08:24:36 +0700

    chore: Step.14-Solution-Add-Header-to-GridLayout

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -45,7 +45,6 @@ class SleepTrackerFragment : Fragment() {
 
         binding.sleepTrackerViewModel = sleepTrackerViewModel
 
-        //binding.setLifecycleOwner(this)
         binding.lifecycleOwner = this
 
         // Add an Observer on the state variable for showing a Snackbar message
@@ -61,7 +60,7 @@ class SleepTrackerFragment : Fragment() {
                 // has a configuration change.
                 sleepTrackerViewModel.doneShowingSnackbar()
             }
-        }
+        })
 
         // Add an Observer on the state variable for Navigating when STOP button is pressed.
         sleepTrackerViewModel.navigateToSleepQuality.observe(viewLifecycleOwner) { night ->
@@ -80,7 +79,7 @@ class SleepTrackerFragment : Fragment() {
                 // has a configuration change.
                 sleepTrackerViewModel.doneNavigating()
             }
-        }
+        })
 
         sleepTrackerViewModel.navigateToSleepDataQuality.observe(viewLifecycleOwner) { night ->
             night?.let {
@@ -90,7 +89,7 @@ class SleepTrackerFragment : Fragment() {
                                 .actionSleepTrackerFragmentToSleepDetailFragment(night))
                 sleepTrackerViewModel.onSleepDataQualityNavigated()
             }
-        }
+        })
 
         val manager = GridLayoutManager(activity, 3)
         binding.sleepList.layoutManager = manager
@@ -112,7 +111,7 @@ class SleepTrackerFragment : Fragment() {
             it?.let {
                 adapter.addHeaderAndSubmitList(it)
             }
-        }
+        })
 
         return binding.root
     }
